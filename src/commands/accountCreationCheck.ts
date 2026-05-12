472a05d312ccc64b26226ec3ae10b99cc3f5ae24
7Tiamatr/x_ <159614599+Synthxyl704@users.noreply.github.com>
2026-05-12 20:17:59 +0530

    ban a member directly if account lifetime less than a month

--- src/commands/accountCreationCheck.ts
+++ src/commands/accountCreationCheck.ts
@@ -1,7 +1,7 @@
 import Eris from 'eris';
 import { Command } from '../types/command';
 
-const MUTE_DURATION = (14) * 24 * 60 * 60 * 1000; 
+// const MUTE_DURATION = (14) * 24 * 60 * 60 * 1000; 
 
 export default (bot: Eris.Client): Command => ({
     name: 'accountCreationCheck',
@@ -12,25 +12,29 @@ export default (bot: Eris.Client): Command => ({
         let now = new Date();
         let diff = now.getTime() - created.getTime();
         let diffDays = Math.floor(diff / (1000 * 60 * 60 * 24));
-        if (diffDays < 60) {
-            const muteUntilDuration = new Date(Date.now() + MUTE_DURATION);
-            await member.edit({ communicationDisabledUntil: muteUntilDuration }, 'Account too new');
-            let dmChannel = await bot.getDMChannel('1216795874877771806'); // DM aris instead
-            dmChannel.createMessage({
-                embeds: [{
-                    color: 0xffffff,
-                    description: `<@${member.id}> (${member.username}) created their account <t:${Math.round(member.createdAt/1000)}:R> (<t:${Math.round(member.createdAt/1000)}:F>)`
-                }],
-                components: [{
-                    type: Eris.Constants.ComponentTypes.ACTION_ROW,
-                    components: [{
-                        label: 'Visit Profile',
-                        style: Eris.Constants.ButtonStyles.LINK,
-                        type: Eris.Constants.ComponentTypes.BUTTON,
-                        url: `https://discord.com/users/${member.id}`
-                    }]
-                }]
-            });
-        }
+        // if (diffDays < 60) {
+        //     const muteUntil = new Date(Date.now() + MUTE_DURATION);
+        //     await member.edit({ communicationDisabledUntil: muteUntil }, 'Account too new');
+        //     let dmChannel = await bot.getDMChannel('1216795874877771806'); // DM aris instead
+        //     dmChannel.createMessage({
+        //         embeds: [{
+        //             color: 0xffffff,
+        //             description: `<@${member.id}> (${member.username}) created their account <t:${Math.round(member.createdAt/1000)}:R> (<t:${Math.round(member.createdAt/1000)}:F>)`
+        //         }],
+        //         components: [{
+        //             type: Eris.Constants.ComponentTypes.ACTION_ROW,
+        //             components: [{
+        //                 label: 'Visit Profile',
+        //                 style: Eris.Constants.ButtonStyles.LINK,
+        //                 type: Eris.Constants.ComponentTypes.BUTTON,
+        //                 url: `https://discord.com/users/${member.id}`
+        //             }]
+        //         }]
+        //     });
+        // }
+        if (diffDays < (30)) {
+            await guild.banMember(member.id, 0, 'Account too new (<1 month.)');
+            // await guild.banMember(member.id, 0, 'Account too new (<1 month, contact 1216795874877771806 in case this was a mistake.)');
+        } 
     }
 });
